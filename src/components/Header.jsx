9b0f2c5d33cbb97765530820c6ba04fb479f2736
dsrkafuu <dsrkafuu@outlook.com>
2021-06-16 11:28:28 +0800

    fix: header escape

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.scss';
 function Header() {
   return (
     <header className={styles.container}>
-      <h1 className={styles.title}>Banker's Algorithm</h1>
+      <h1 className={styles.title}>Banker&apos;s Algorithm</h1>
       <span className={styles.author}>B18030620 张承扬</span>
     </header>
   );
